cf8eae5621a0e49f298526797938594ad71d297e
Simon Marty <martysi@amazon.com>
2022-05-09 15:36:32 -0700

    Add Redshift driver

--- src/main/java/com/amazonaws/secretsmanager/util/Config.java
+++ src/main/java/com/amazonaws/secretsmanager/util/Config.java
@@ -67,8 +67,11 @@ public final class Config {
     private static Properties loadPropertiesFromConfigFile(String resourceName) {
         Properties newConfig = new Properties(System.getProperties());
 
-        try (InputStream configFile = ClassLoader.getSystemResourceAsStream(resourceName)) {
-            if (configFile != null) {
+        InputStream configFile;
+
+        try {
+            configFile = ClassLoader.getSystemResourceAsStream(resourceName);
+            if(configFile != null) {
                 newConfig.load(configFile);
                 configFile.close();
             }
